6195869bc3dbe2b1e0b10a0a86c0b9afa6fdf1b5
pontaGC <makizawawa@gmail.com>
2025-05-13 00:22:29 +0900

    Add debugger step thouth.

--- CopyPaste/CSharp/Sevice.Core/Extensions/ColorExtensions.cs
+++ CopyPaste/CSharp/Sevice.Core/Extensions/ColorExtensions.cs
@@ -1,14 +1,17 @@
-﻿using System.Drawing;
+﻿using System.Diagnostics;
+using System.Drawing;
 
 namespace Sevices.Core.Extensions
 {
     public static class ColorExtensions
     {
+        [DebuggerStepThrough]
         public static string ToHex(this Color color)
         {
             return $"#{color.R:X2}{color.G:X2}{color.B:X2}";
         }
 
+        [DebuggerStepThrough]
         public static string ToHexWithAlpha(this Color color)
         {
             return $"#{color.A:X2}{color.R:X2}{color.G:X2}{color.B:X2}";
