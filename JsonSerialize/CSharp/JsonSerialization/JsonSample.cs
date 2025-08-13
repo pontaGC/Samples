e4a8b2286abce36c4e217e0245986deb4047786f
pontaGC <makizawawa@gmail.com>
2025-08-14 00:59:20 +0900

    Added gif.

---
+++ JsonSerialize/CSharp/JsonSerialization/JsonSample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace JsonSerialization
+{
+    [Serializable]
+    public class JsonSample
+    {
+        [JsonPropertyName("Type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("Content")]
+        public JsonSampleContent Content { get; set; }
+    }
+
+    public class JsonSampleContent
+    {
+        [JsonPropertyName("Name")]
+        public string Name { get; set; }
+    }
+}
